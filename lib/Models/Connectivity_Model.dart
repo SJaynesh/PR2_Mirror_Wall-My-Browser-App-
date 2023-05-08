c4c2b91fc9ceed0624e1ab2e02f3b7cb3c2c0bae
Jaynesh Sarkar <115562979+SJaynesh@users.noreply.github.com>
2023-05-08 15:12:48 +0530

    Delete Connectivity_Model.dart

--- lib/Models/Connectivity_Model.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-class Connectivity_Model {
-  String status;
-  StreamSubscription? connectivityStream;
-
-  Connectivity_Model({required this.status, this.connectivityStream});
-}
